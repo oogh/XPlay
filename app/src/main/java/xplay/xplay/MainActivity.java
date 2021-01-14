dfc74eeda1f0ab5454877eb5e51df2e6ab1d38c9
Andy <andy@gaoding.com>
2021-01-14 16:21:55 +0800

    修改后提交

--- app/src/main/java/xplay/xplay/MainActivity.java
+++ app/src/main/java/xplay/xplay/MainActivity.java
@@ -1,85 +1,43 @@
-/*******************************************************************************
-**                                                                            **
-**                     Jiedi(China nanjing)Ltd.                               **
-**	               创建：夏曹俊，此代码可用作为学习参考                       **
-*******************************************************************************/
-
-/*****************************FILE INFOMATION***********************************
-**
-** Project       : FFmpeg
-** Description   : FFMPEG项目创建示例
-** Contact       : xiacaojun@qq.com
-**        博客   : http://blog.csdn.net/jiedichina
-**		视频课程 : 网易云课堂	http://study.163.com/u/xiacaojun		
-				   腾讯课堂		https://jiedi.ke.qq.com/				
-				   csdn学院		http://edu.csdn.net/lecturer/lecturer_detail?lecturer_id=961	
-**                 51cto学院	http://edu.51cto.com/lecturer/index/user_id-12016059.html	
-** 				   下载最新的ffmpeg版本 ffmpeg.club
-**                 
-**   安卓流媒体播放器 课程群 ：23304930 加入群下载代码和交流
-**   微信公众号  : jiedi2007
-**		头条号	 : 夏曹俊
-**
-*******************************************************************************/
-//！！！！！！！！！ 加群23304930下载代码和交流
-
-
 package xplay.xplay;
 
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.SeekBar;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements Runnable,SeekBar.OnSeekBarChangeListener {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
-    // Used to load the 'native-lib' library on application startup.
+public class MainActivity extends AppCompatActivity implements Runnable, SeekBar.OnSeekBarChangeListener {
+
+    private static final int REQUEST_PERMISSION = 1;
+
     static {
-        System.loadLibrary( "native-lib" );
+        System.loadLibrary("native-lib");
     }
 
     private Button bt;
     private SeekBar seek;
     private Thread th;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate( savedInstanceState );
-
-        //去掉标题栏
-        supportRequestWindowFeature( Window.FEATURE_NO_TITLE);
-        //全屏，隐藏状态
-        getWindow().setFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN ,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
-        //屏幕为横屏
-        setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE );
-
-
-        setContentView( R.layout.activity_main );
-        bt = findViewById( R.id.open_button );
-        seek = findViewById( R.id.aplayseek );
+        super.onCreate(savedInstanceState);
+        checkPermission();
+        setContentView(R.layout.activity_main);
+        bt = findViewById(R.id.open_button);
+        seek = findViewById(R.id.aplayseek);
         seek.setMax(1000);
-        seek.setOnSeekBarChangeListener( this );
+        seek.setOnSeekBarChangeListener(this);
 
-        bt.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.e("XPlay","open button click!");
-                //打开选择路径窗口
-                Intent intent = new Intent();
-                intent.setClass( MainActivity.this ,OpenUrl.class);
-                startActivity( intent );
-
-
-            }
-        } );
+        bt.setOnClickListener(view -> {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this, OpenUrl.class);
+            startActivity(intent);
+        });
         //启动播放进度线程
         th = new Thread(this);
         th.start();
@@ -88,17 +46,18 @@ public class MainActivity extends AppCompatActivity implements Runnable,SeekBar.
     //播放进度显示
     @Override
     public void run() {
-        for(;;)
-        {
-            seek.setProgress((int)(PlayPos()*1000));
+        for (; ; ) {
+            seek.setProgress((int) (PlayPos() * 1000));
             try {
-                Thread.sleep( 40 );
+                Thread.sleep(40);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
+
     public native double PlayPos();
+
     public native void Seek(double pos);
 
     @Override
@@ -113,6 +72,34 @@ public class MainActivity extends AppCompatActivity implements Runnable,SeekBar.
 
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
-        Seek( (double)seekBar.getProgress()/(double)seekBar.getMax() );
+        Seek((double) seekBar.getProgress() / (double) seekBar.getMax());
+    }
+
+    private boolean checkPermission() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(
+                this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE,
+                            android.Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_PERMISSION);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION) {
+            if (!checkPermission()) {
+                Toast.makeText(this, "权限不完整", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
